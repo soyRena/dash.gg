a1d99197df0635d70883af67f2f1d3862ebfb49e
soyRena <rennanfelipers@gmail.com>
2022-01-18 18:34:14 -0300

    feat: created the project architecture

---
+++ src/hooks/use-fetch.ts
@@ -0,0 +1,12 @@
+import useSWR, { SWRConfig } from 'swr'
+
+export function useFetch<Data = any>(url: string) {
+   const { data, error} = useSWR<Data>(url, async url => {
+      const response = await fetch(url);
+      const data = await response.json();
+      
+      return data
+   })
+
+   return { data, error }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+   colors: {
+      background: '#2c2c2c',
+      text: '#fff',
+      primary: '#8257e6'
+   }
+}
+
+export default theme
